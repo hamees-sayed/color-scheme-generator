8f6a5670d702453d76d60b563edfe79aab4643dd
Hamees Sayed <hameessayed71@gmail.com>
2022-12-01 21:18:22 +0530

    removed hardcoded value and made it more dynamic

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ btn.addEventListener("click", getColorScheme)
 function getColorScheme() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor()}&mode=${schemeValue()}&count=5`)
         .then(resp => resp.json())
-        .then(data => {
-            fillColor(data)
-        })
+        .then(data => fillColor(data))
 }
 
 function fillColor(data) {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < data.colors.length; i++) {
         const colorHex = data.colors[i].hex.value
 
         document.getElementById(`bar${i}`).style.background = colorHex
@@ -25,7 +23,6 @@ function seedColor() {
     let seedColor = document.getElementById("seed-color").value
     return seedColor.slice(1)
 }
-
 
 function schemeValue() {
     const scheme = document.getElementById("color-scheme")
